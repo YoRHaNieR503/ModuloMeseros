c5e8d2b75c4efb70d70ac68ce638cf77740e7636
Marroquin1 <luis.marroquin@catolica.edu.sv>
2024-04-18 12:33:38 -0600

    Controlador y vista orden

---
+++ Controllers/OrdenController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModuloMeseros.Models;
+
+namespace ModuloMeseros.Controllers
+{
+    public class OrdenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
