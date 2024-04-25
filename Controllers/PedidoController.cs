23fd2722b3975ff6fbbc726f3d68d61afc81d8d3
Marroquin1 <luis.marroquin@catolica.edu.sv>
2024-04-25 00:49:52 -0600

    Creacion de vista y controller del prototipo 3

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModuloMeseros.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
